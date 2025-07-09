0b1f266762fb483983d9f4a46c09b509543c950f
Afaf Afridi <ibrahim.hasan00710@gmail.com>
2025-07-09 15:59:16 +0500

    number commit

---
+++ app.js
@@ -0,0 +1,20 @@
+    document.write("<h3>Counting:</h3>");
+    for (let i = 1; i <= 15; i++) {
+      document.write(i + ", ");
+    }
+    document.write("<h3>Reverse counting:</h3>");
+    for (let i = 10; i >= 1; i--) {
+      document.write(i + ", ");
+    }
+       document.write("<h3>Even:</h3>");
+    for (let i = 0; i <= 20; i += 2) {
+      document.write(i + ", ");
+    }
+    document.write("<h3>Odd:</h3>");
+    for (let i = 1; i < 20; i += 2) {
+      document.write(i + ", ");
+    }
+    document.write("<h3>Series:</h3>");
+    for (let i = 2; i <= 20; i += 2) {
+      document.write(i + "k, ");
+    }
